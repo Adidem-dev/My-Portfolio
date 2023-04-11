a39f680993d70edaaa931d12a45105f121393e0f
Adidem-dev <ayoadeusmanademola@gmail.com>
2023-04-11 12:26:01 +0100

    Add Javascript DOM

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+let btnMenu = document.getElementById('hamburger');
+let xBtn = document.getElementById("x-mark");
+btnMenu.addEventListener('click',myFunction);
+function myFunction(){
+    let element = document.querySelector(".header-nav");
+    let list = document.getElementById('menu-option');
+    element.style.display = "none";
+    list.style.display = "block";
+}
+xBtn.addEventListener('click', closing);
+function closing(){
+    let element = document.querySelector(".header-nav");
+    let list = document.getElementById('menu-option');
+    element.style.display = "flex";
+    list.style.display = "none";
+}
+let btn1 = document.getElementById('about-link');
+let btn2 = document.getElementById('contact-link');
+let btn3 = document.getElementById('port');
+btn1.addEventListener('click', closing);
+btn2.addEventListener('click',closing);
+btn3.addEventListener('click',closing);
+
+
+
 

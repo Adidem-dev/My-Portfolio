a0f6b83008a4307da0607d168cc2f3591e59ff15
Adidem-dev <ayoadeusmanademola@gmail.com>
2023-04-14 15:06:53 +0100

    Modify CSS stlying

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const button = document.querySelector('.hamburger');
-const element = document.querySelector('.nav-bar_desktop');
-const list = document.querySelectorAll('.nav-bar_desktop a, .nav-bar_desktop img');
+const element = document.querySelector('.nav-bar-mobile');
+const close = document.querySelector('.x-mark');
+const list = document.querySelectorAll('.nav-bar-mobile a, .x-mark')
 const overlay = document.querySelector('.overlay');
-
+const { body } = document;
 button.addEventListener('click', () => {
   element.style.display = 'flex';
   button.style.display = 'none';
+  close.style.display = 'block';
   overlay.classList.remove('hidden');
+  body.style.overflow = 'hidden';
 });
 
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener('click', () => {
     element.style.display = 'none';
     button.style.display = 'block';
-    overlay.classList.add('hidden');
+    close.style.display = 'none';
+    overlay.classList.add('hidden')
+    body.style.overflow = 'scroll';
   })
 }

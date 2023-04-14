e5891c72598b18c9144f5f83cb463e180a5c9a9c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-04-14 11:10:27 +0300

    Update form valiadtion

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ for (let i = 0; i < list.length; i++) {
 
 const email = document.getElementById('mail');
 const form = document.getElementById('form1');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  if (email.value !== email.value.toLowerCase()) {
+    document.getElementById('error').innerText = 'Error, Email must be lowercase. Form is not sent';
+  } else {
+    document.getElementById('error').innerText = '';
+    form.submit();
+  }
+});

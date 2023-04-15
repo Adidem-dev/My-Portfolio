cee77d037af2fed24b3920da78a916c445e6a98d
Adidem-dev <ayoadeusmanademola@gmail.com>
2023-04-15 11:24:57 +0100

    Modify CSS and Javascript

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const button = document.querySelector('.hamburger');
 //const element = document.querySelector('.nav-bar-mobile');
 const close = document.querySelector('.x-mark');
-const list = document.querySelectorAll('.nav-bar-mobile')
+const menuList = document.querySelector('.nav-bar-mobile');
+const list = document.querySelectorAll('.nav-bar-mobile a');
 const overlay = document.querySelector('.overlay');
 const { body } = document;
 
 button.addEventListener('click', () => {
-  list.style.display = 'flex';
+  menuList.style.display = 'flex';
   button.style.display = 'none';
   close.style.display = 'block';
   overlay.classList.remove('hidden');
   body.style.overflow = 'hidden';
 });
 
+const quit = () => {
+  menuList.style.display = 'none';
+    button.style.display = 'block';
+    close.style.display = 'none';
+    overlay.classList.add('hidden');
+    body.style.overflow = 'scroll';
+}
+  close.addEventListener('click', () => {
+    quit();
+  })
 
-  // list.addEventListener('click', () => {
-  //   element.style.display = 'none';
-  //   button.style.display = 'block';
-  //   close.style.display = 'none';
-  //   overlay.classList.add('hidden')
-  //   body.style.overflow = 'scroll';
-  // })
+if (window.innerWidth < 767) {
+  list.forEach((item) => {
+    item.addEventListener('click', () => {
+    quit ();
+    });
+  });
+}

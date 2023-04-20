03d06bff35c693b73252d36681587a4ddcfbf613
Adidem-dev <ayoadeusmanademola@gmail.com>
2023-04-20 09:22:16 +0100

    Update changes to HTML and JS files

--- index.js
+++ index.js
@@ -33,17 +33,9 @@ if (window.innerWidth < 767) {
   });
 }
 
-/* Form Validation Section */
 
-const email = document.getElementById('mail');
-const form = document.getElementById('form1');
+        
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (email.value !== email.value.toLowerCase()) {
-    document.getElementById('error').innerText = 'Error, Email must be lowercase. Form is not sent';
-  } else {
-    document.getElementById('error').innerText = '';
-    form.submit();
-  }
-});
+          
+
+
